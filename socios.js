ee400f3762b8d32abe08abb8e331592a7e5d10fd
JenniRC <jenniteleco@gmail.com>
2016-04-20 21:17:27 +0200

    Added files via upload

---
+++ socios.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	var contador_msgs = 0;
+	function printMsgs (msgs, id){
+        for (var i in msgs){
+			$(id).prepend("<div>" + msgs[i].content + "<li>" + msgs[i].date + "</li>" + "</div>");
+            $(id).prepend("<div>" + '<img id="img" src='+ msgs[i].av + '>' + "<br/>" + msgs[i].author + ", Asunto:  " + msgs[i].title + "  <div>");
+        }
+        $(id).accordion({collapsible: true, active:false, heightStyle: "content"});
+    };
+
+	$("#container").tabs();
+
+	$.getJSON("timeline.json").done(function(data) {
+		console.log("timeline");
+		printMsgs(data.Msgs, "#msgs");
+	}).fail(function(){
+		alert("No se han podido cargar los mensajes");
+	});
+
+	$.getJSON("myline.json").done(function(data) {
+		console.log("myline");
+		printMsgs(data.Msgs, "#msgs2");
+	}).fail(function(){
+		alert("No se han podido cargar mis mensajes");
+	});
+
+	$("#update").click(function(){
+		$.getJSON("update.json").done(function(data) {
+			console.log("update");
+			if(contador_msgs == 0){
+				contador_msgs++;
+				printMsgs(data.Msgs, "#newMsgs");
+			}else{
+				alert("no hay ningun mensaje nuevo");
+			}
+		}).fail(function(){
+			alert("No se han podido cargar los nuevos mensajes");
+		});
+	});
+
+
+});
